8f3c458ddcad76a84736b78ca3f09886a3cb707b
Allout004 <spencerddoty@gmail.com>
2021-09-20 22:16:29 -0700

    search bar no work

--- assets/script/testing.js
+++ assets/script/testing.js
@@ -1,32 +1,33 @@
 
-$(document).ready(function () {
-  $('input.autocomplete').autocomplete({
-    data: {
-      "Chris Evans": null,
-      "Scarlett Johansson": null,
-      "Robert Downey": 'https://m.media-amazon.com/images/M/MV5BZmUyMDEyOTgtZmUwOS00NTdkLThlNzctNTM1ODQ4M2VhMjdhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
-      // "InsertNameHere": "addImageHereORNull",
-      // "InsertNameHere": "addImageHere",
-      // "InsertNameHere": "addImageHere",
-      // "InsertNameHere": "addImageHere",
-      // "InsertNameHere": "addImageHere",
-      // "InsertNameHere": "addImageHere",
-      // "InsertNameHere": "addImageHere",
-      // "InsertNameHere": "addImageHere",
-    },
-    limit: 3,
-  });
-});
+// $(document).ready(function () {
+//   $('input.autocomplete').autocomplete({
+//     data: {
+//       "Chris Evans": null,
+//       "Scarlett Johansson": null,
+//       "Robert Downey": 'https://m.media-amazon.com/images/M/MV5BZmUyMDEyOTgtZmUwOS00NTdkLThlNzctNTM1ODQ4M2VhMjdhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
+//       // "InsertNameHere": "addImageHereORNull",
+//       // "InsertNameHere": "addImageHere",
+//       // "InsertNameHere": "addImageHere",
+//       // "InsertNameHere": "addImageHere",
+//       // "InsertNameHere": "addImageHere",
+//       // "InsertNameHere": "addImageHere",
+//       // "InsertNameHere": "addImageHere",
+//       // "InsertNameHere": "addImageHere",
+//     },
+//     limit: 3,
+//   });
+// });
 // onAutocomplete()
 // console.log("suh")
 //  need to import data dynamically or thru API call on page load
 var searchBar = document.getElementById("search")
 // Execute a function when the user releases a key on the keyboard
 $(document).submit(function (event) {
-  console.log("This is a the submit function working")
+  // console.log("This is a the submit function working")
   event.preventDefault();
   // Get value from text field.
   const searchString = $('#search').val();
+  console.log(searchString)
   // call searchForActor()
   searchForActor(searchString);
 })
